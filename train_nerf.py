6e3ce83a57c26c88d1034c080eb8ecc5c316bb2a
nakuramino <minorunakura@gmail.com>
2022-02-21 03:39:24 +0000

    positional enc

--- train_nerf.py
+++ train_nerf.py
@@ -11,7 +11,7 @@ def train_simple_image(im_path, logger_name, steps):
         check_val_every_n_epoch=10)
     train_dl = dataloader.getPhotoDataloader(im_path, batch_size=4096, num_workers=8, shuffle=True)
     val_dl = dataloader.getValDataloader(im_path, batch_size=1, num_workers=1, shuffle=False)
-    model = nerf_model.ImageNeRFModel(position_dim=-1)
+    model = nerf_model.ImageNeRFModel(position_dim=10)
     trainer.fit(model, train_dl, val_dl)
 
 if __name__ == '__main__': 
