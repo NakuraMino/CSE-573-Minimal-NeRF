0bf31333305598d13fe5cd6edaa064ac41912840
nakuramino <minorunakura@gmail.com>
2022-02-21 04:26:10 +0000

    position_dim as arg

--- train_nerf.py
+++ train_nerf.py
@@ -5,13 +5,13 @@ import dataloader
 import nerf_model 
 import argparse
 
-def train_simple_image(im_path, logger_name, steps): 
+def train_simple_image(im_path, logger_name, steps, pos_enc): 
     wandb_logger = WandbLogger(name=f"{logger_name}", project="NeRF")
     trainer = Trainer(gpus=1, default_root_dir="/home/nakuram/CSEP573-NeRF/experiments/", max_steps=steps, logger=wandb_logger,
         check_val_every_n_epoch=10)
     train_dl = dataloader.getPhotoDataloader(im_path, batch_size=4096, num_workers=8, shuffle=True)
-    val_dl = dataloader.getValDataloader(im_path, batch_size=1, num_workers=1, shuffle=False)
-    model = nerf_model.ImageNeRFModel(position_dim=10)
+    val_dl = dataloader.getValDataloader(im_path, batch_size=1, num_workers=8, shuffle=False)
+    model = nerf_model.ImageNeRFModel(position_dim=pos_enc)
     trainer.fit(model, train_dl, val_dl)
 
 if __name__ == '__main__': 
@@ -20,6 +20,8 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--steps', type=int, default=100000, help='max number of steps')
     parser.add_argument('-i', '--im_path', type=str, default='./tests/test_data/grad_lounge.png',
         help='The image path to use as data')
+    parser.add_argument('-p', '--position_encoding', type=int, default=10, help='position encoding length')
     
     args = parser.parse_args()
-    train_simple_image(args.im_path, args.name, args.steps)
+    train_simple_image(args.im_path, args.name, args.steps, args.position_encoding)
+
